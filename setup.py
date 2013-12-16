31a940bb7a2078247d2cc5b21ef6891cfe11e17f
Kimmo Brunfeldt <kimmo.brunfeldt@futurice.com>
2013-12-16 18:47:12 +0200

    Rename readme back to markdown

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 """Nap provides simple and easy way to request HTTP API resources. Stop coding needless code."""
 #!/usr/bin/env python
 
+import os
 from distutils.core import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
     name='nap',
     version='0.1.0',
     description=__doc__,
+    long_description=read('README.md'),
     license='MIT',
     install_requires=[
         "requests"
