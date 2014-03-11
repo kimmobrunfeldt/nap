14cc034cc0a621dcc901fd4c81f4824fa27a7d6b
Kimmo Brunfeldt <kimmo.brunfeldt@futurice.com>
2014-03-12 00:34:47 +0200

    Remove unneeded args parsing

--- setup.py
+++ setup.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python
 
-import os
-import sys
-
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
-    sys.exit()
 
 readme = """Read docs from GitHub_
 
