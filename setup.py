7c844c42853e93d44aa2c2582af6b33905e8837f
Kimmo Brunfeldt <kimmo.brunfeldt@futurice.com>
2014-03-12 00:40:43 +0200

    Bump version number to 0.1.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ readme = """Read docs from GitHub_
 
 setup(
     name='nap',
-    version='0.1.0',
+    version='0.1.1',
     description='Clean way to request HTTP API resources',
     long_description=readme,
     author='Kimmo Brunfeldt',
