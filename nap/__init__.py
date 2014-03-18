c01397644543b70330b664c8336cb137f6825eeb
Kimmo Brunfeldt <kimmo.brunfeldt@futurice.com>
2014-03-18 22:23:08 +0200

    Bump version number to 0.2.3

--- nap/__init__.py
+++ nap/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'Kimmo Brunfeldt'
 __email__ = 'kimmobrunfeldt@gmail.com'
 __url__ = 'https://github.com/kimmobrunfeldt/nap'
-__version__ = '0.2.2-dev'
+__version__ = '0.2.3'
 
 from . import api
