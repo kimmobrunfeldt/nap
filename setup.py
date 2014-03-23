650c68be44d396609e1388a6d9dccea66cf76475
Kimmo Brunfeldt <kimmo.brunfeldt@futurice.com>
2014-03-23 23:48:59 +0200

    Set development version: 1.0.0-dev

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ readme = """Read docs from GitHub_
 
 setup(
     name='nap',
-    version='1.0.0',
+    version='1.0.0-dev',
     description='Convenient way to request HTTP APIs',
     long_description=readme,
     author='Kimmo Brunfeldt',
