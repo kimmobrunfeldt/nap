4ace6bd3e2a4035ed4563032d7d32360083daa1b
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2014-09-20 16:05:42 +0300

    Set development version: 2.0.0-dev

--- nap/__init__.py
+++ nap/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'Kimmo Brunfeldt'
 __email__ = 'kimmobrunfeldt@gmail.com'
 __url__ = 'https://github.com/kimmobrunfeldt/nap'
-__version__ = '2.0.0'
+__version__ = '2.0.0-dev'
 
 from . import url
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ readme = """Read docs from GitHub_
 
 setup(
     name='nap',
-    version='2.0.0',
+    version='2.0.0-dev',
     description='Convenient way to request HTTP APIs',
     long_description=readme,
     author='Kimmo Brunfeldt',
