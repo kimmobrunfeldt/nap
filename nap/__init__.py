e73fbf1a59edb9b09efd16ba21f43fb13082bf0a
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2014-09-20 16:05:37 +0300

    Bump version number to 2.0.0

--- nap/__init__.py
+++ nap/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'Kimmo Brunfeldt'
 __email__ = 'kimmobrunfeldt@gmail.com'
 __url__ = 'https://github.com/kimmobrunfeldt/nap'
-__version__ = '1.0.1-dev'
+__version__ = '2.0.0'
 
 from . import url
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ readme = """Read docs from GitHub_
 
 setup(
     name='nap',
-    version='1.0.1-dev',
+    version='2.0.0',
     description='Convenient way to request HTTP APIs',
     long_description=readme,
     author='Kimmo Brunfeldt',
